5e846a6848290f1cd1337c6861a98d957e9e131b
haywhyzero <aregbeayomide@gmail.com>
2026-06-22 06:32:17 +0100

    fix bugs

--- test.dart
+++ test.dart
@@ -1,5 +1,5 @@
 void main() {
-    String? name = "adejumoke";
+    String? name = "Adejumoke";
     int? age = 23;
     String? nationality = "nigerian";
     String? religion = "islam";
@@ -10,10 +10,29 @@ void main() {
     String? favColor = "custard blue";
     String? hobby = "watching tvd";
 
+
+    int year = DateTime.now().year;
+    int day = DateTime.now().day;
+    int month = DateTime.now().month;
+    String todaysDate = "${day}st/$month/$year";
+
+
     String? paragraph;
     paragraph = """Hello, my name is $name. I am $age years old. I am a $nationality and I practice $religion.
      I am from $stateOfOrigin but I live in $address. I am a $occupation. 
      My fav food is $favFood and my fav color is $favColor. I love $hobby when I am free""";
 
-     print(paragraph);
+
+    String ayoParagraph = "\tHello, my name is $name. I am ${age + 5} years old. I am a ${nationality.toUpperCase()} \and I practice $religion. \nI am from $stateOfOrigin but I live in $address. \nToday's date is $todaysDate";
+
+    // Club game
+
+ 
+
+    if (age < 25) {
+      print("You are not allowed in this club");
+    } else {
+      print("Welcome, $name");
+    }
+                        
 }
